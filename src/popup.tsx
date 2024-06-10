1e49033217a2e0a12ff15cbf331e8515199d02f9
brennenho <brennen.ho@gmail.com>
2024-06-09 20:50:32 -0700

    course capacity formatting and placeholder ui

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,22 +1,111 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 import "boxicons/css/boxicons.min.css"
 import "./style.css"
+import "@fontsource/open-sans/700.css"
+import "@fontsource/inter/600.css"
+import "@fontsource/inter/400.css"
 
 function IndexPopup() {
   return (
     <div className="popup">
-      <h1>
-        <i className="bx bxs-flag-alt"></i> Conquest
-      </h1>
-      <footer>
-        Crafted with <i className="bx bxs-heart"></i> by{" "}
-        <a href="https://brennen.dev" target="_blank">
-          Brennen
-        </a>
-      </footer>
+      <div className="header">
+        <h1>COURSES</h1>
+      </div>
+      <div className="header-bar"></div>
+      <div className="courses">
+        <CourseBox
+          courseTag="CSCI"
+          courseNum="104"
+          prof="John Doe"
+          days="MW"
+          start="11:00 AM"
+          end="12:30 PM"
+        />
+        <CourseBox
+          courseTag="CSCI"
+          courseNum="104"
+          prof="John Doe"
+          days="MW"
+          start="11:00 AM"
+          end="12:30 PM"
+        />
+        <CourseBox
+          courseTag="CSCI"
+          courseNum="104"
+          prof="John Doe"
+          days="MW"
+          start="11:00 AM"
+          end="12:30 PM"
+        />
+        <CourseBox
+          courseTag="CSCI"
+          courseNum="104"
+          prof="John Doe"
+          days="MW"
+          start="11:00 AM"
+          end="12:30 PM"
+        />
+        <CourseBox
+          courseTag="CSCI"
+          courseNum="104"
+          prof="John Doe"
+          days="MW"
+          start="11:00 AM"
+          end="12:30 PM"
+        />
+      </div>
     </div>
   )
 }
 
 export default IndexPopup
+
+interface CourseContainerProps {
+  courseTag: string
+  courseNum: string
+  prof: string
+  days: string
+  start: string
+  end: string
+}
+
+const CourseBox: React.FC<CourseContainerProps> = ({
+  courseTag,
+  courseNum,
+  prof,
+  days,
+  start,
+  end
+}) => {
+  return (
+    <div className="course-container">
+      <CourseIcon courseTag={courseTag} />
+      <div className="info">
+        <h3>
+          {courseTag} {courseNum}
+        </h3>
+        <h4>{prof}</h4>
+      </div>
+      <div className="time">
+        <p>{days}</p>
+        <p>Start: {start}</p>
+        <p>End: {end}</p>
+      </div>
+    </div>
+  )
+}
+
+interface CourseIconProps {
+  courseTag: string
+}
+
+const CourseIcon: React.FC<CourseIconProps> = ({ courseTag }) => {
+  if (courseTag === "CSCI") {
+    return (
+      <div className="icon viterbi">
+        <i className="bx bx-code-alt"></i>
+      </div>
+    )
+  }
+}
