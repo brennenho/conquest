f9575f04c0b77a0caebb7ca52003817c648ffc17
brennenho <brennen.ho@gmail.com>
2024-06-16 17:35:32 -0700

    improved formatting on watchlist button

--- src/ui/components/watchlistButton.tsx
+++ src/ui/components/watchlistButton.tsx
@@ -14,15 +14,23 @@ const WatchlistButton: React.FC<WatchlistButtonProps> = ({
 }) => {
   const [label, setLabel] = React.useState("Add to watchlist")
   const className = "btn btn-default add-to-course-bin col-xs-12"
-  const styleDefault = { marginTop: "2px" }
-  const styleWatching = { marginTop: "2px", backgroundColor: "black" }
+  const styleDefault = {
+    marginTop: "2px",
+    backgroundColor: "white",
+    color: "#666666"
+  }
+  const styleWatching = {
+    marginTop: "2px",
+    backgroundColor: "#8c1a11",
+    color: "white"
+  }
+  const [style, setStyle] = React.useState(styleDefault)
   const watchlistManager = new WatchlistManager()
 
   React.useEffect(() => {
     const checkWatchlist = async () => {
       const status = await watchlistManager.getWatchlistStatus(sectionId)
       if (status && status === "watching") {
-        console.log("SET LABEL FOR ", sectionId)
         setLabel("Remove from watchlist")
       }
     }
@@ -33,12 +41,14 @@ const WatchlistButton: React.FC<WatchlistButtonProps> = ({
   const onClick = async () => {
     if (label === "Remove from watchlist") {
       await watchlistManager.removeFromWatchlist(sectionId, setLabel)
+      setStyle(styleDefault)
     } else {
       await watchlistManager.addToWatchlist(sectionId, department, setLabel)
+      setStyle(styleWatching)
     }
   }
   return (
-    <button onClick={onClick} className={className} style={styleDefault}>
+    <button onClick={onClick} className={className} style={style}>
       {label}
     </button>
   )
